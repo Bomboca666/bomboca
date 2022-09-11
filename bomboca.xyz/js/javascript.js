7414fb9dc931c287549210576c2fe2a41221b206
Bomboca666 <bomboca666>
2022-09-11 22:26:22 +0100

    version 0.2

--- bomboca.xyz/js/javascript.js
+++ bomboca.xyz/js/javascript.js
@@ -7,21 +7,7 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-// Automatic Slideshow - change image every 3 seconds
-var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
-}
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName) {
   var i, x;
